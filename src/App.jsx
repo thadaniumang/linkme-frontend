be684331aab240d3d4edb81da34b76574045d0e4
Umang Thadani <umang.thadani@somaiya.edu>
2022-05-03 00:06:17 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello Vite + React!</p>
+        <p>
+          <button className="px-2 py-3 text-yellow-300 bg-gray-600" type="button" onClick={() => setCount((count) => count + 2)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.jsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
