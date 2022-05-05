0fc2e1d9ad88633a93d238eb888823a171acd374
Umang_Thadani <thadaniumang@outlook.com>
2022-05-05 23:31:10 +0530

    UI Changes

--- src/Authentication/Login.jsx
+++ src/Authentication/Login.jsx
@@ -96,7 +96,7 @@ const Login = () => {
                          {
                             error && 
                             (
-                                <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                                <div class="my-4 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
                                     <strong class="font-bold">Invalid Credentials!</strong>
                                     <span class="block sm:inline">Please Login Again</span>
                                 </div>
