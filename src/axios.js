35d2abc261f0b572089ac5dacf64f4e5dc15da85
Umang_Thadani <thadaniumang@outlook.com>
2022-05-05 22:52:35 +0530

    API Base updated

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://127.0.0.1:8000/";
+const baseURL = "https://linkme-backend-production.up.railway.app/"
 
 const axiosInstance = axios.create({
   baseURL: baseURL,
